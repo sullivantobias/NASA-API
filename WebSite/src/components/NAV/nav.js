543343264d19fca6a651348dbc51963ba5c7895b
Sullivan Tobias <stobias@sqli.com>
2019-02-11 14:53:45 +0100

    Underline Navigation

--- WebSite/src/components/NAV/nav.js
+++ WebSite/src/components/NAV/nav.js
@@ -1,6 +1,7 @@
 /**
  * NAV component
  */
+import $ from 'jquery';
 
 export default {
   mounted() {
@@ -14,5 +15,37 @@ export default {
         navBar.classList.remove('scrolled');
       }
     });
+    var $el,
+        leftPos,
+        newWidth,
+        $mainNav = $(".navbar-nav");
+
+    $mainNav.append("<li id='magic-line'></li>");
+    var $magicLine = $("#magic-line");
+
+    $magicLine
+    .width($(".active").width())
+    .css("left", $(".active a").position().left)
+    .data("origLeft", $magicLine.position().left)
+    .data("origWidth", $magicLine.width());
+
+    $(".navbar-nav li a").hover(
+        function () {
+          $el = $(this);
+          leftPos = $el.position().left;
+          newWidth = $el.parent().width();
+          $magicLine.stop().animate({
+            left: leftPos,
+            width: newWidth
+          });
+        },
+        function () {
+          $magicLine.stop().animate({
+            left: $magicLine.data("origLeft"),
+            width: $magicLine.data("origWidth")
+          });
+        }
+    );
+
   },
 };
