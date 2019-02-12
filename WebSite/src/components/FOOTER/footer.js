9cc0952fa16ebb28c2bff7fbfaea87b53b4d9ce2
Sullivan Tobias <stobias@sqli.com>
2019-02-12 10:56:23 +0100

    Footer Update

--- WebSite/src/components/FOOTER/footer.js
+++ WebSite/src/components/FOOTER/footer.js
@@ -3,4 +3,22 @@
  */
 
 export default {
+  data() {
+    return {
+      links: {
+        Contact: {
+          classLink: 'link col-4 contact',
+          title: "Contact Me"
+        },
+        Nasa: {
+          classLink: 'link col-4 website',
+          title: "Nasa Website"
+        },
+        Api: {
+          classLink: 'link col-4 api',
+          title: "Nasa API"
+        }
+      }
+    }
+  }
 };
